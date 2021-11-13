52a8180184bcae310a7f8cc9ebf3adbee6c239d9
mattiapesce <91664050+mattiapesce@users.noreply.github.com>
2021-11-13 12:01:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-using namespace std;
+using namespace std ;
 
 int main() {
-   return 0;
+    int a=1 , b=0 , c=0 , n=0 , i=0 ;
+    cin >> n ;
+    b=i ;
+    if (n!=0){
+        cout << "1" << endl ;
+    }
+    for(int i=1 ; i<=n ; i++) {
+        c=(b+a) ;
+        b=a ;
+        a=c ;
+        if(c<=n) {
+            cout << c << endl ;
+        }
+    }
+    return 0;
 }
